b1e42018c54c56a63fd4ff74fb63d519a5953fae
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-06-04 13:02:32 +0300

    Add files via upload

---
+++ 3.Advanced_conditions/2.Lab/4.personalTitles.js
@@ -0,0 +1,27 @@
+function personalTitle (input){
+    let age = Number(input[0]);
+    let sex = input[1];
+
+    if (sex=="m"){
+        if (age>=16){
+            console.log("Mr." );
+        }else{
+            console.log("Master");
+        }
+    }else if (sex=="f"){
+        if (age>=16){
+            console.log("Ms." );
+        }else{
+            console.log("Miss");
+        }
+    }
+}
+
+personalTitle (["12",
+"f"])
+personalTitle (["17",
+"m"])
+personalTitle (["25",
+"f"])
+personalTitle (["13.5",
+"m"])
