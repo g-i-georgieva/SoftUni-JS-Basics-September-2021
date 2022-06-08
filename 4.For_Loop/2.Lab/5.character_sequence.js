58da2a3894d5ec781247755561c5481e2ad24063
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-06-08 10:25:35 +0300

    Add files via upload

---
+++ 4.For_Loop/2.Lab/5.character_sequence.js
@@ -0,0 +1,11 @@
+function characterSequence(text) {
+    let word = text[0]
+  
+    for (let i = 0; i < word.length; i++) {
+        console.log(word[i]);
+
+    }
+
+}
+characterSequence(["softuni"])
+characterSequence(["ice cream"])
