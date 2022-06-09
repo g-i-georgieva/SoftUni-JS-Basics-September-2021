b10adbcfcf38c23a4d48c6bba19431af06dfab65
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-06-09 13:16:58 +0300

    Add files via upload

---
+++ 4.For_Loop/3.Exercise/2.multiplication_table.js
@@ -0,0 +1,10 @@
+function solve (input){
+    let num=Number(input[0]);
+
+    for (let i = 1; i <= 10; i++){
+        console.log(`${i} * ${num} = ${num*i}`);    
+    }
+}
+
+solve (["5"])
+solve (["3"])
