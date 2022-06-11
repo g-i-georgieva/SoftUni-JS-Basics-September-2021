9b075318e404be4cd5dc4b6f09b85004c3f4573c
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-06-11 17:54:14 +0300

    Add files via upload

---
+++ 4.For_Loop/4.More Exercises/10.clock2.js
@@ -0,0 +1,13 @@
+function input() {
+    for (let h = 0; h < 24; h++) {
+        for (let min = 0; min < 60; min++) {
+            for (let sec = 0; sec < 60; sec++) {
+                console.log(`${h} : ${min} : ${sec}`)
+                
+            }
+
+        }
+
+    }
+}
+input()
