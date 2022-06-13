4b06c61f7298bb66964848f39ad1ed2a1d68495c
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-06-13 09:30:17 +0300

    Add files via upload

---
+++ 5.While_Loop/2.Lab/2.Password.js
@@ -0,0 +1,23 @@
+function password (input){
+    let username = input[0];
+    let pass=input[1];
+    let data = input[2];
+    index=3
+
+    while (data!==pass) {
+        data=input[index];
+        index++
+        
+    }
+
+    console.log(`Welcome ${username}!`);
+}
+
+password (["Nakov",
+"1234",
+"Pass",
+"1324",
+"1234"])
+password (["Gosho",
+"secret",
+"secret"])
