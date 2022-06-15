015b04bc11dc16f0fc4ff4dec929a4f1f5011283
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-06-15 09:05:06 +0300

    Add files via upload

---
+++ 5.While_Loop/4. More Exercises/4.numbers-Divided-by3.js
@@ -0,0 +1,12 @@
+function numbersDevidedBy3() {
+    let i = 1
+    while (i <= 100) {
+        if (i % 3 === 0){
+            console.log(i)
+        }
+
+        i++
+    }
+}
+
+numbersDevidedBy3([])
