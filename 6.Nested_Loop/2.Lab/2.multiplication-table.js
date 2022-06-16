ca1aaf82531c4e35d38c75e52d751dd8848996b9
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-06-16 09:51:21 +0300

    Add files via upload

---
+++ 6.Nested_Loop/2.Lab/2.multiplication-table.js
@@ -0,0 +1,10 @@
+function table (){
+    for (let first = 1; first <=10; first++) {
+        for (let second = 1; second <= 10; second++) {
+            console.log(`${first} * ${second} = ${first*second}`)            
+        }
+        
+    }
+}
+
+table()
